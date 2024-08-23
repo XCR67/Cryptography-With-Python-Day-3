46ce909c2524aedd24439a3c8cea000c0569e898
XCR67 <33xaviercamacho@gmail.com>
2024-08-23 01:22:01 -0700

    Symmetric Encryption Bones.py

--- Main(5) (1).py
+++ Main(5) (1).py
@@ -1,58 +1,68 @@
 #DAY 3 - SYMMETRIC ENCRYPTION
-#Dependencies
-import random
-import string
+#Dependencies (random and string
+
 #Converts readable text into Hexadecimal
-def conHex(message):
-  result = ""
-  for i in range(len(message)):
-    result += str(hex(ord(message[i]))) #Convert its to ascii > Hexadecimal > String
-  return result
+#def conHex(message):
+  #Make result string
+
+  #Loop through each letter
+    #Convert the ascii value of the letter into hexadecimal and add it to result by casting it to a string
+
+  #Return the result
+
+
 #Reverts the Hexadecimal string back into readable text
-def revHex(hexmessage):
-  result = ""
-  letter = 2
-  while letter < len(hexmessage): #Go though every letter in the message
-    tenplc = int(hexmessage[letter], 16) * 16
-    onesplc = int(hexmessage[letter + 1], 16) 
-    result += chr(tenplc + onesplc) #Converts sum into its letter equivalent using ascii
-    letter += 4 #Move to the next letter
-  return result
+#def revHex(hexmessage):
+  #Make result string and a letter variable at 2
+
+  #Go though every 2 digit hex value in the message using a while loop
+    #Convert the tens place from hexadecimal to decimal using int(hex, 16) * 16
+
+    #Convert the ones place from hexadecimal to decimal using int(hex, 16
+
+    #Converts sum of the ones place and tens place into its letter equivalent using chr()
+
+    #Move to the next set of 2 digit hex value
+
+  #Return the result
+
+
 #Encrypts the message by adding the key to the message
-def encryptSymm(message, key):
-  #Variable initializationa nd converts key and message to encryptable hex values
-  message = conHex(message)
-  key = conHex(key)
-  index = 2
-  cipherText = ""
-  while index < len(message) - 1: #Go thorugh eac letter in message
-    #Swap hex values of message and key by adding their tens and ones places
-    ten = int(message[index], 16) * 16 + int(key[index + 1], 16)
-    one = int(message[index + 1], 16) + int(key[index], 16) * 16
-    cipherText += chr(ten + one) #Converts sum into its letter equivalent using ascii
-    index += 4
-  return cipherText
+#def encryptSymm(message, key):
+  #Variable initialization and converts key and message to encryptable hex values
+
+  #Also make result string and a letter variable at 2
+
+  #Go though every 2 digit hex value in the message using a while loop
+    #Add the hex decimal value of the tens place of msg with the decimal 
+    #value of the ones place of key and vice versa
+
+    #Converts sum of the math done above into its letter equivalent using ascii
+
+    #Move to the next set of 2 digit hex value
+
+  #Return the result
+
+
 #Decrypts the encrypted text with the same key
-def decryptSymm(cipherText, key):
-  #Variable initializationa nd converts key and message to decryptable hex values
-  cipherText = conHex(cipherText)
-  key = conHex(key)
-  index = 2
-  message = ""
-  while index < len(cipherText) - 1: #Go thorugh eac letter in message
-    #Revert hex values of message and key by subtracting their tens and oens places
-    ten = int(cipherText[index], 16) * 16 - int(key[index + 1], 16)
-    one = int(cipherText[index + 1], 16) - int(key[index], 16) * 16
-    message += chr(ten + one) #Converts sum into its letter equivalent using ascii
-    index += 4
-  return message
+#def decryptSymm(cipherText, key):
+  #Variable initialization and converts key and cipherText to decryptable hex values
+
+  #Also make result string and a letter variable at 2
+
+  #Go though every 2 digit hex value in the message using a while loop
+    #Add the hex decimal value of the tens place of cipherText with the decimal 
+    #value of the ones place of key and vice versa (cipheText is always subtracted by key
+
+    #Converts sum into its letter equivalent using ascii
+
+    #Move to the next set of 2 digit hex value
+
+  #Return the result
+
 #Encryption and Key Generation
-message = input("Enter your secret message: ")
-key = input("Enter a key of at least length " + str(len(message)) + ":")
-while len(key) < len(message): #Makes sure the key is as long as the message
-  key = input("Key must be at least length " + str(len(message)) + ":")
-print("Key is " + key)
-cipherText = encryptSymm(message, key)
-print(cipherText)
+
+#Make sure the key is as long as the message
+
 #Decryption
-print(decryptSymm(cipherText, key))
+
